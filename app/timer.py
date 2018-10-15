56a5780db30aa865490b4cd72815f8e2156a173d
Danila <doronin1303@bk.ru>
2018-10-15 21:07:29 +0300

    HW3_correction

--- app/timer.py
+++ app/timer.py
@@ -2,7 +2,8 @@ import json
 import datetime
 def wsgi_timer(env, start_resp):
     now = str(datetime.datetime.now().strftime("<%d-%m-%Y %H:%M>"))
-    url = env['RAW_URI']
+    print(env)
+    url = env['REMOTE_ADDR'] + env['PATH_INFO']
     start_resp('200 OK', [('Content-Type', 'application/json')])
     arr = {"time": now, "url": url}
     resp = json.dumps(arr)
